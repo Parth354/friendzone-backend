6ff40555c5f63e0ab0a319892076cebe98a87894
Parth Bajaj <120926651+Parth354@users.noreply.github.com>
2025-01-05 22:45:32 +0530

    Updated cors

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import cookieParser from 'cookie-parser';
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://friendzone-pi.vercel.app/',
     credentials: true
 }))
 app.use(cookieParser());
